eb518ed4301ec7ec275845fe9c27617dd0548873
michaeldai1006 <dtc74110@gmail.com>
2020-04-02 00:06:41 -0400

    index route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.use('/', function(req, res, next) {
+  // Log req content
+  console.log('==================================================');
+  console.log('Method:', req.method);
+  console.log('--------------------------------------------------');
+  console.log('Headers:', req.headers);
+  console.log('--------------------------------------------------');
+  console.log('Body:', req.body);
+  console.log('==================================================');
+
+  // Response
+  res.json({
+    status: true,
+    message: 'Copy that'
+  });
 });
 
 module.exports = router;
